7d7d2db0f1fbffe2bc414fe3b9f8776219bd613f
itsmedanka <itsmekali321@gmail.com>
2021-12-31 14:16:16 +0530

    added android-iamge-search.py

--- android-image-search.py
+++ android-image-search.py
@@ -1,43 +1,50 @@
+import pyshorteners
 from selenium import webdriver
-from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.firefox.options import Options
 from selenium.webdriver.common.by import By
 import time
-from itertools import cycle
+import os
+import pyshorteners as sh
 
 print("open IP Webcam app and scan where to search")
 print("REMEMBER TO BE IN SAME NETWORK OF YOUR LAPTOP!")
 print("Welcome to android-image-search")
 options = Options()
 options.add_argument("--headless")
-browser = webdriver.Firefox()
+browser = webdriver.Firefox(options=options)
 browser.get("http://192.168.0.72:8080/jsfs.html")
 print("went to the site")
 time.sleep(5)
+os.system("rm image_search.png")#removes already saved file
+time.sleep(2)
 browser.save_screenshot("image_search.png")
+print("got image")
 time.sleep(3)
 browser.get("https://www.google.com/imghp?hl=en")
+print("went to the site")
 time.sleep(2)
 browser.find_element(By.XPATH, "/html/body/div[1]/div[3]/form/div[1]/div[1]/div[1]/div[2]/div[3]/div[2]/span").click()
 time.sleep(2)
 browser_url = browser.current_url
 browser.find_element(By.XPATH, "/html/body/div[1]/div[3]/div/div[2]/form/div[1]/div/a").click()
 time.sleep(2)
-browser.find_element(By.ID, "awyMjb").send_keys("/home/yash/Python/image_search.png")
-time.sleep(10)
-
-if browser.current_url == browser_url:
-    print("Loading...",end="\n")
-    for i in cycle(["|", "/", "-", "\ "]):
-        a = 0
-        print(i, end='\r')
-        time.sleep(0.2)
-        if a > 10:
-            print("image is hard to find")
-            exit()
-
+browser.find_element(By.ID, "awyMjb").send_keys(os.getcwd()+"/image_search.png")
+print("searching for image...")
+for i in range(10):
+    print("|", end='\r')
+    time.sleep(0.2)
+    print("/", end='\r')
+    time.sleep(0.2)
+    print("-", end='\r')
+    time.sleep(0.2)
+    print("\ ", end='\r')
+    time.sleep(0.2)
+time.sleep(5)
+print(" ", end="\r")
+if browser_url == browser.current_url:
+    print("Image is hard to find")
 else:
-        print(browser.current_url)
+    print(pyshorteners.Shortener().tinyurl.short(browser.current_url))
 
 
 
